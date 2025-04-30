b7a8c394af4d1bff169e0ca60271ce6a7268ece0
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-04-30 15:48:08 +0200

    Change buttons tabs placements

--- src/pages/Projet.jsx
+++ src/pages/Projet.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import ToolBox from "./projets-ecole/BoiteOutil";
 import ComGraph from "./projets-ecole/ComGraph";
@@ -30,25 +29,24 @@ export const Projects = () => {
 
   return (
     <div className="p-4">
-      {/* Choix de catégorie */}
+
       <div className="flex gap-4 mb-6">
         <button
-          className={`cursor-pointer px-4 py-2 rounded ${activeBox === "ecole" ? "bg-orange-400 text-white" : "bg-gray-200"}`}
+          className={`cursor-pointer px-4 py-2 rounded ${activeBox === "ecole" ? "bg-orange-400 text-white" : "bg-gray-200 text-black"}`}
           onClick={() => setActiveBox("ecole")}
         >
           Projets École
         </button>
         <button
-          className={`cursor-pointer px-4 py-2 rounded ${activeBox === "perso" ? "bg-green-500 text-white" : "bg-gray-200"}`}
+          className={`cursor-pointer px-4 py-2 rounded ${activeBox === "perso" ? "bg-green-500 text-white" : "bg-gray-200 text-black" }`}
           onClick={() => setActiveBox("perso")}
         >
           Projets Personnels
         </button>
       </div>
 
-      {/* Liste des projets */}
       {activeBox === "ecole" && (
-        <div className="space-x-4 mb-4">
+        <div className="space-x-4 mb-4 text-center place-content-between">
           <button onClick={() => setActiveProject("BoiteOutil")} className={`cursor-pointer px-2 py-1 transition duration-200 ${activeProject === "BoiteOutil" ? "text-orange-400 font-semibold" : "text-gray-50 hover:text-orange-300 hover:underline underline-offset-2" }`}>Boite à outil numérique</button>
           <button onClick={() => setActiveProject("ComGraph")} className={`cursor-pointer px-2 py-1 transition duration-200 ${activeProject === "ComGraph" ? "text-orange-400 font-semibold" : "text-gray-50 hover:text-orange-300 hover:underline underline-offset-2" }`}>Communication Graphique</button>
           <button onClick={() => setActiveProject("ComWeb")} className={`cursor-pointer px-2 py-1 transition duration-200 ${activeProject === "ComWeb" ? "text-orange-400 font-semibold" : "text-gray-50 hover:text-orange-300 hover:underline underline-offset-2" }`}>Communication Web</button>
@@ -57,13 +55,12 @@ export const Projects = () => {
       )}
 
       {activeBox === "perso" && (
-        <div className="space-x-4 mb-4">
+        <div className="space-x-4 mb-4 text-center place-content-between">
           <button onClick={() => setActiveProject("DevWeb")} className={`cursor-pointer px-2 py-1 transition duration-200 ${activeProject === "DevWeb" ? "text-orange-400 font-semibold" : "text-gray-50 hover:text-orange-300 hover:underline underline-offset-2" }`}>Développement Web</button>
           <button onClick={() => setActiveProject("Photo")} className={`cursor-pointer px-2 py-1 transition duration-200 ${activeProject === "Photo" ? "text-orange-400 font-semibold" : "text-gray-50 hover:text-orange-300 hover:underline underline-offset-2" }`}>Photographie</button>
         </div>
       )}
 
-      {/* Contenu du projet sélectionné */}
       <div className="mt-6">{renderProjectComponent()}</div>
     </div>
   );
