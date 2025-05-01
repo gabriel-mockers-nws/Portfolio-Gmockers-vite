4421a5af4ca441e70bfa01e3822371f338ec439a
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-05-01 14:58:22 +0200

    add external condition

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,20 +1,29 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-export default function MyButton({ href, children, className = '', ...props }) {
+export default function Button({ href, children, className = '', isExternal = false, ...props }) {
     const baseStyle = `inline-flex items-center justify-center bg-orange-400 hover:bg-orange-500 text-white px-6 py-3 my-5 rounded-full transition-colors duration-300 ${className}`;
 
     if (href) {
-        return (
-            <a
-                href={href}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={baseStyle}
-                {...props}
-            >
-                {children}
-            </a>
-        );
+        if (isExternal) {
+            return (
+                <a
+                    href={href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={baseStyle}
+                    {...props}
+                >
+                    {children}
+                </a>
+            );
+        } else {
+            return (
+                <Link to={href} className={baseStyle} {...props}>
+                    {children}
+                </Link>
+            );
+        }
     }
 
     return (
@@ -24,8 +33,9 @@ export default function MyButton({ href, children, className = '', ...props }) {
     );
 }
 
-MyButton.propTypes = {
+Button.propTypes = {
     href: PropTypes.string,
     children: PropTypes.node.isRequired,
     className: PropTypes.string,
+    isExternal: PropTypes.bool,
 };
