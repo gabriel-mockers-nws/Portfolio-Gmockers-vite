f761d099fbb0f59deb1c71b2ecb0bd12b940e1e4
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-05-01 14:59:58 +0200

    Change all artciles with Article components

--- src/pages/projets-perso/DevWeb.jsx
+++ src/pages/projets-perso/DevWeb.jsx
@@ -1,5 +1,4 @@
-import SwiperCarousel from "../../components/SwiperCarousel";
-import Button from "../../components/Button";
+import Article from "../../components/Article"; 
 
 const LeBonpoil = [
     "/projets/DevWeb/Le-bon-poil/mainpage1.png",
@@ -24,62 +23,90 @@ export const DevWeb = () => {
 
     return (
         <>
-        <div className="flex flex-row items-center gap-6 my-8">
-            <SwiperCarousel images={LeBonpoil} />
-            <div className="w-[60%]">
-                <h4 className="text-2xl font-semibold mb-2">Formation HTML et débuts CSS</h4>
-                <p className="text-base leading-relaxed">
-                    Ce projet représente mes débuts dans mon apprentissage personnel de l&apos;HTMl et du CSS. Il s&apos;agit d&apos;un site pour un restaurant fictif. <br />
-                    Mes principales difficultées étaient la découverte des deux langages qui, à ce moment, était totalement nouveau pour moi.  <br />
-                    <br />
-                    Ce projet à été fait en suivant un cours sur internet <em>&#40;Grafikart HTML&#41;</em>. Cela m&apos;a permis de comprendre les bases de ces deux langages et la construction d&apos;un site web.
-                </p>
-                <Button href={"https://github.com/gabriel-mockers-nws/Restaurant-Le-Bon-Poil"} target="_blank">Lien du projet &nbsp; <i className="fa-brands fa-github"/></Button>
-            </div>
-        </div>
+        <Article
+            title="Formation HTML et débuts CSS"
+            imageType="carousel"
+            images={LeBonpoil}
+            reverse={false}
+            buttons={[
+                {
+                label: "Voir le projet",
+                href: "https://github.com/gabriel-mockers-nws/Restaurant-Le-Bon-Poil",
+                iconClass: "fa-brands fa-github",
+                isExternal: true
+                }
+            ]}
+            >
+                Ce projet représente mes débuts dans mon apprentissage personnel de l&apos;HTMl et du CSS. Il s&apos;agit d&apos;un site pour un restaurant fictif. <br />
+                Mes principales difficultées étaient la découverte des deux langages qui, à ce moment, était totalement nouveau pour moi.  <br />
+                <br />
+                Ce projet à été fait en suivant un cours sur internet <em>&#40;Grafikart HTML&#41;</em>. Cela m&apos;a permis de comprendre les bases de ces deux langages et la construction d&apos;un site web.
+        </Article>
 
-        <div className="flex flex-row-reverse items-center gap-6 my-8">
-            <SwiperCarousel images={Blog} />
-            <div className="w-[60%]">
-                <h4 className="text-2xl font-semibold mb-2">Confirmation HTML et CSS</h4>
-                <p className="text-base leading-relaxed">
-                    Ce second projet m&apos;a permis de me confimer dans le langage HTMl et d&apos;en apprendre bien plus sur le CSS. Ses classes, flex et grid, etc. Comment bien structurer une page web en s&apos;appyant sur une maquette Figma.<br />
-                    Mes principales difficultées étaient la découverte du CSS et de ses spécificités. <br />
-                    <br />
-                    Ce projet à été fait en suivant un cours sur internet <em>&#40;Grafikart CSS&#41;</em>. Cela m&apos;a permis de comprendre les spécificité du langage CSS  et les possibilités de style d&apos;un site web.
-                </p>
-                <Button href={"https://github.com/gabriel-mockers-nws/Demoblog"} target="_blank">Lien du projet &nbsp;<i className="fa-brands fa-github"/></Button>
-            </div>
-        </div>
+    
+        <Article
+            title="Confirmation HTML et CSS"
+            imageType="carousel"
+            images={Blog}
+            reverse={true}
+            buttons={[
+                {
+                label: "Voir le projet",
+                href: "https://github.com/gabriel-mockers-nws/Demoblog",
+                iconClass: "fa-brands fa-github",
+                isExternal: true
+                }
+            ]}
+            >
+                 Ce second projet m&apos;a permis de me confimer dans le langage HTMl et d&apos;en apprendre bien plus sur le CSS. Ses classes, flex et grid, etc. Comment bien structurer une page web en s&apos;appyant sur une maquette Figma.<br />
+                Mes principales difficultées étaient la découverte du CSS et de ses spécificités. <br />
+                <br />
+                Ce projet à été fait en suivant un cours sur internet <em>&#40;Grafikart CSS&#41;</em>. Cela m&apos;a permis de comprendre les spécificité du langage CSS  et les possibilités de style d&apos;un site web.
+        </Article>
 
-        <div className="flex flex-row items-center gap-6 my-8">
-            <SwiperCarousel images={Mini_project} />
-            <div className="w-[60%]">
-                <h4 className="text-2xl font-semibold mb-2">Entrainement au JavaScript</h4>
-                <p className="text-base leading-relaxed">
-                    Avec ce projet réalisé à la suite du cours JavaScript <em>&#40;Grafikart JS&#41;</em> je désirait m&apos;entrainer avec ce nouveau langage. J&apos;ai donc demander à un ami de donner un projet à réaliser <br />
-                    <br /> 
-                    J&apos;ai donc eu à réaliser un formuaire d&apos;entrée de noms dans une liste, en utilisant les langages HTML et JavaScript. <br />
-                    <br />
-                    Mes principales difficultés ont été la découverte du langage JavaScript ainsi que la compréhension des fonctions, qui étaient particulièrement difficiles pour moi.
-                </p>
-                <Button href={"https://github.com/gabriel-mockers-nws/Mini-projets"} target="_blank">Lien du projet &nbsp;<i className="fa-brands fa-github"/></Button>
-            </div>
-        </div>
+        <Article
+            title="Entrainement au JavaScript"
+            imageType="carousel"
+            images={Mini_project}
+            reverse={false}
+            buttons={[
+                {
+                label: "Voir le projet",
+                href: "https://github.com/gabriel-mockers-nws/Mini-projets",
+                iconClass: "fa-brands fa-github",
+                isExternal: true
+                }
+            ]}
+            >
+                Avec ce projet réalisé à la suite du cours JavaScript <em>&#40;Grafikart JS&#41;</em> je désirait m&apos;entrainer avec ce nouveau langage. J&apos;ai donc demander à un ami de donner un projet à réaliser <br />
+                <br /> 
+                J&apos;ai donc eu à réaliser un formuaire d&apos;entrée de noms dans une liste, en utilisant les langages HTML et JavaScript. <br />
+                <br />
+                Mes principales difficultés ont été la découverte du langage JavaScript ainsi que la compréhension des fonctions, qui étaient particulièrement difficiles pour moi.
+        </Article>
 
-        <div className="flex flex-row-reverse items-center gap-6 my-8">
-            <img src="/projets/DevWeb/vite+react.png" alt="Action Marketing" className="h-[27vh] w-[60vh] "/>
-            <div className="w-[60%]">
-                <h4 className="text-2xl font-semibold mb-2">Réalisation portfolio</h4>
-                <p className="text-base leading-relaxed">
-                    Ce projet est mon plus gros projet personnel qui est aussi pour ma formation. Il s&apos;agit du portage de mon ancien portfolio réalisé avec WordPress en langage de développement. Pour le réalisé, j&apos;ai choisit d&apos;utiliser React et de le styliser avec tailwindCSS 4.1. Il me sera utile pour ma soutenance de fin d&apos;année et mon passage en deuxième année option développement. <br />
-                    <br /> 
-                    Mes principales difficultés rencontrées ont été la découverte de React ainsi que de Tailwind, me donnant un véritable défit à la réalisation de ce projet. <br /> 
-                    J&apos;ai choisit ces deux framework pour leurs simplification du code, et du chargement des pages, la possibilité de les lier entres-elles, comme l&apos;atteste cette page projet. 
-                </p>
-                <Button href={"https://github.com/gabriel-mockers-nws/Portfolio-Gmockers-vite"} target="_blank">Lien du projet &nbsp;<i className="fa-brands fa-github"/></Button>
-            </div>
-        </div>
+
+        <Article
+            title="Entrainement au JavaScript"
+            imageType="image"
+            images="/projets/DevWeb/vite+react.png"
+            reverse={true}
+            buttons={[
+                {
+                label: "Voir le projet",
+                href: "https://github.com/gabriel-mockers-nws/Portfolio-Gmockers-vite",
+                iconClass: "fa-brands fa-github",
+                isExternal: true
+                }
+            ]}
+            >
+                Avec ce projet réalisé à la suite du cours JavaScript <em>&#40;Grafikart JS&#41;</em> je désirait m&apos;entrainer avec ce nouveau langage. J&apos;ai donc demander à un ami de donner un projet à réaliser <br />
+                <br /> 
+                J&apos;ai donc eu à réaliser un formuaire d&apos;entrée de noms dans une liste, en utilisant les langages HTML et JavaScript. <br />
+                <br />
+                Mes principales difficultés ont été la découverte du langage JavaScript ainsi que la compréhension des fonctions, qui étaient particulièrement difficiles pour moi.
+        </Article>
+
         </>
     );
 }
