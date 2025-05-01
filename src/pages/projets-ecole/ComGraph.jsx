836c4eddd797aa3e14767efa3df735f026919245
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-05-01 16:31:04 +0200

    Change div by Article component

--- src/pages/projets-ecole/ComGraph.jsx
+++ src/pages/projets-ecole/ComGraph.jsx
@@ -1,4 +1,4 @@
-import SwiperCarousel from "../../components/SwiperCarousel";
+import Article from "../../components/Article";
 
 const ImagesUX_UI = [
     "/projets/ComGraph/BDEShop/maquette.png",
@@ -12,15 +12,14 @@ export const ComGraph = () => {
 
     return (
         <>
-        <div className="flex flex-row items-center gap-6 my-8">
-            <SwiperCarousel images={ImagesUX_UI} />
-            <div  className="w-[60%]">
-                <h4 className="text-xl font-semibold mb-2">BDE Shop</h4>
-                <p  className="text-base leading-relaxed">
-                    Dans ce projet, nous devion faire le wireframe &#40;Image 1&#41; ainsi que le maquettage d&apos;un sitre de e-commerce de vente pour des goodies du BDE de la Normandie Web School. Pour ce faire, nous avons appris à utiliser Figma. <br /> &#40;Voir projet Marketing pour voir la réalisation de ce site&#41;
-                </p>
-            </div>
-        </div>
+        <Article
+            title="BDE Shop"
+            imageType="carousel"
+            images={ImagesUX_UI}
+            reverse={false}
+            >
+                Dans ce projet, nous devion faire le wireframe &#40;Image 1&#41; ainsi que le maquettage d&apos;un sitre de e-commerce de vente pour des goodies du BDE de la Normandie Web School. Pour ce faire, nous avons appris à utiliser Figma. <br /> &#40;Voir projet Marketing pour voir la réalisation de ce site&#41;
+        </Article>
         </>
     );
 }
--- src/pages/projets-ecole/Marketing.jsx
+++ src/pages/projets-ecole/Marketing.jsx
@@ -1,44 +1,44 @@
 
+import Article from "../../components/Article";
 
 export const Marketing = () => {
 
     return (
         <>
-        <div className="flex flex-row items-center gap-8 my-8" >
-            <img src="/projets/Marketing/Persona.svg" alt="Présentation-persona" className="h-[27vh] w-[60vh]  " />
-            <div  className="w-[60%]">
-                <h4 className="text-xl font-semibold mb-2">Persona</h4>
-                <p className="text-base leading-relaxed">
+        <Article
+                title="Persona"
+                imageType="image"
+                images="/projets/Marketing/Persona.svg"
+                reverse={false}
+                >
                     Ce projet représente la création d&apos;un persona, la compréhension des attentes d&apos;une cible, connaitre ses besoins, ses craintes, et comment bien la représenter pour adapter notre campagne de communication.
-                </p>
-            </div>
-        </div>
-        
-        <div className="flex flex-row-reverse items-center gap-6 my-8">
-            <img src="/projets/Marketing/action-marketing.png" alt="Action Marketing" className="h-[27vh] w-[60vh] "/>
-            <div className="w-[60%]">
-                <h4 className="text-xl font-semibold mb-2">Action Marketing</h4>
-                <p className="text-base leading-relaxed">
-                    Réalisation d&apos;une études marketing pour la société GreenWey. Cette étude m&apos;a permis de comprendre comment une action marketing est étudiée avant de la mettre en place.
-                </p>
-                <button className="flex-0.5 bg-orange-400 rounded-full hover:bg-orange-500 px-8 py-3 mx-2 focus:bg-amber-600 text-center" >
-                    <a href="https://www.canva.com/design/DAGba5z4tPo/X7pvPWmWuby2yWPIWkIPqg/edit" target="_blank">
-                    Lien du projet
-                    </a> 
-                    <i className="fa-solid fa-diagram-project"/>
-                </button>
-            </div>
-        </div>
+        </Article>
 
-        <div className="flex flex-row items-center gap-8 my-8" >
-            <img src="/projets/Marketing/BDEShopD3.png" alt="Présentation-persona" className="h-[27vh] w-[60vh]  " />
-            <div  className="w-[60%]">
-                <h4 className="text-xl font-semibold mb-2">BDE Shop</h4>
-                <p className="text-base leading-relaxed">
+        <Article
+            title="Action Marketing"
+            imageType="image"
+            images="/projets/Marketing/action-marketing.png"
+            reverse={true}
+            buttons={[
+                {
+                label: "Voir le projet",
+                href: "https://www.canva.com/design/DAGba5z4tPo/X7pvPWmWuby2yWPIWkIPqg/edit",
+                iconClass: "fa-solid fa-diagram-project",
+                isExternal: true
+                }
+            ]}
+            >
+                Réalisation d&apos;une études marketing pour la société GreenWey. Cette étude m&apos;a permis de comprendre comment une action marketing est étudiée avant de la mettre en place.
+        </Article>
+
+        <Article
+                title="BDE Shop"
+                imageType="image"
+                images="/projets/Marketing/BDEShopD3.png"
+                reverse={false}
+                >
                     Nous avions pour devoir de réaliser le site de vente du BDE de la Normandie Web school à partir de la maquette figma conçue auparavant &#40;voir projet UX/UI dans l&apos;onglet communication graphique&#41;. Le tout en respectant une charte graphique déjà existante.
-                </p>
-            </div>
-        </div>
+        </Article>
         </>
     );
 }
