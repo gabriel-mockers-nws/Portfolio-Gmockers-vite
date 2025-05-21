cbda4c1fe1fc4903fe26dfbb4778031b34ccd176
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-05-21 10:25:59 +0200

    Case directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import  Header  from "./components/Header";
-import  Footer  from "./components/footer";
+import  Footer  from "./components/Footer";
 // import { createBrowserRouter } from 'react-router';
 import './styles.css'
 import Home from './pages/home';
