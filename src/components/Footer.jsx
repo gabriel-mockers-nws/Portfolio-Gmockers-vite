8e2010aca8887c0204bcb93b65be945080b0000f
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-06-06 11:33:08 +0200

    add background and lightmod background

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 export const Footer =  () => {
     return (
         <>
-            <div className="py-5 px-10 flex relative items-center ">
-                <div className="py-5 flex items-center absolute left-10">
+            <div className="py-5 px-10 flex relative items-center h-20 bg-gradient-to-r from-purple-500 to-pink-500  light:bg-linear-to-r light:from-cyan-500 light:to-blue-500 ">
+                <div className="py-5 flex items-center absolute left-10 ">
                     <p className="flex items-center">
                         Projet réalisé avec Vite &nbsp;<img src="img/vite.svg" alt="vite-logo" /> + React&nbsp; <img src="img/react.svg" alt="react-logo" /> 
                     </p>
