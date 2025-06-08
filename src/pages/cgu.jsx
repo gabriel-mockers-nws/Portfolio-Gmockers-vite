f7b3ffb93a4fed39edaaa53fd0d946b7dc07bba2
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-06-08 11:29:11 +0200

    Add cgu page

---
+++ src/pages/cgu.jsx
@@ -0,0 +1,10 @@
+
+export const CGU = () => {
+    return (
+        <>
+        <h2 className="text-center m-19 text-3xl font-Orbitron font-black">Condition générale d&apos;utilisisation</h2>
+        </>
+    );
+};
+
+export default CGU; 
