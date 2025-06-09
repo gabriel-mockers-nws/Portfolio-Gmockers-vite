8e1677975fb95e91cf8b84369a54a8e2ba14da5d
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-06-09 19:08:54 +0200

    modify list style

--- src/components/TextArticle.jsx
+++ src/components/TextArticle.jsx
@@ -8,7 +8,7 @@ export default function TextArticle({
         <>
             <div className="mx-15 my-12">
                 <h4 className="text-2xl font-semibold mb-5 font-Orbitron underline ">{title}</h4>
-                <div className="text-base leading-relaxed mb-4 ml-3 mt-9">{children}</div>
+                <div className="text-base leading-relaxed mb-4 ml-3 mt-9 custom-list">{children}</div>
             </div>
         </>
     );
