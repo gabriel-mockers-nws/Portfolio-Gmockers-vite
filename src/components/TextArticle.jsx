8fb319a75876586c7cd0507c2fad8a529e841d59
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-06-09 12:30:54 +0200

    Create article without images components

---
+++ src/components/TextArticle.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+
+export default function TextArticle({
+    title,
+    children,
+}) {
+    return (
+        <>
+            <div className="mx-15 my-12">
+                <h4 className="text-2xl font-semibold mb-5 font-Orbitron underline ">{title}</h4>
+                <div className="text-base leading-relaxed mb-4 ml-3 mt-9">{children}</div>
+            </div>
+        </>
+    );
+};
+
+TextArticle.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+}; 
