941a962489f2b3c4b3a4dbb4bfd7f1ff88a8d0ee
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2025-06-10 00:27:19 +0200

    Add colorBox component

--- src/components/CharteArticle.jsx
+++ src/components/CharteArticle.jsx
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
+import ColorBox from './ColorBox';
 
 export default function CharteArticle({
     title,
     images = null,
     children,
+    colors = null,
 }) {
     return (
         <>
@@ -18,9 +20,22 @@ export default function CharteArticle({
                 />
                 )}
                 </div>
+
                 <div className='w-full md:w-[65%] text-center md:text-left ml-25'>
                     <h4 className="text-2xl font-semibold mb-5 font-Orbitron underline ">{title}</h4>
                     <div className="text-base leading-relaxed mb-4 custom-list">{children}</div>
+
+                    {colors && colors.length > 0 && (
+                    <div className="flex flex-col items-center md:grid md:grid-cols-3 gap-4 mt-8 ">
+                        {colors.map((color, index) => (
+                            <ColorBox
+                                key={index}
+                                hex={color.hex}
+                                gradient={color.gradient || false}
+                            />
+                        ))}
+                    </div>
+                )}
                 </div>
             </div>
         </>
@@ -32,4 +47,10 @@ CharteArticle.propTypes = {
     title: PropTypes.string.isRequired,
     images: PropTypes.string, 
     children: PropTypes.node.isRequired,
+        colors: PropTypes.arrayOf(
+        PropTypes.shape({
+            hex: PropTypes.string.isRequired,
+            gradient: PropTypes.bool,
+        })
+    ),
 };
