a0a7cdd5d15f1fac13c33a57fd10a509a6755b10
gabriel-mockers-nws <gmockers@normandiewebschool.fr>
2026-01-12 14:19:13 +0100

    Modify projects placements

--- src/pages/projet.jsx
+++ src/pages/projet.jsx
@@ -2,48 +2,49 @@ import Article from "../components/Article";
 
 export const Projects = () => {;
   return (
-    <>
-    <Article
-        title="Projets déveleppement web"
-        imageType="image"
-        images="/img/Developpement/vite+react.png"
-        reverse={false}
-        buttons={[
-            {
-            label: "Voir le projet",
-            href: "https://github.com/gabriel-mockers-nws/Portfolio-Gmockers-vite",
-            iconClass: "fa-brands fa-github",
-            isExternal: true
-            },
-            {
-              label: "Voir plus de projets developpement",
-              href: "/DevProjects",
-              }
-        ]}
-        >
-          Dans cette partie je vous présente tous mes projet de développement web. <br /> 
-    </Article>
+    <>  
 
-    <Article
-        title="Mes autres projets"
-        imageType="image"
-        images="/img/otherProjects/action-marketing.png"
-        reverse={true}
-        buttons={[
-            {
-            label: "Voir le projet",
-            href: "https://www.canva.com/design/DAGba5z4tPo/X7pvPWmWuby2yWPIWkIPqg/edit",
-            iconClass: "fa-solid fa-diagram-project",
-            isExternal: true
-            },
-            {
-              label: "Voir plus d'autres projets",
-              href: "/OtherProjects",
-            }
-        ]}
-        >
-            Dans cette partie je vous présente tous mes autres projets qui sont principalement des projet d&apos;école. <br />
-    </Article>
+      <Article
+      title="Projets marketing & communication digital"
+      imageType="image"
+      images="/img/otherProjects/action-marketing.png"
+      reverse={false}
+      buttons={[
+          {
+          label: "Voir le projet",
+          href: "https://www.canva.com/design/DAGba5z4tPo/X7pvPWmWuby2yWPIWkIPqg/edit",
+          iconClass: "fa-solid fa-diagram-project",
+          isExternal: true
+          },
+          {
+            label: "Voir plus d'autres projets",
+            href: "/OtherProjects",
+          }
+      ]}
+      >
+          Dans cette partie je vous présente tous mes projets marketing et communication digital. <br />
+      </Article>
+
+      <Article
+          title="Projets déveleppement web"
+          imageType="image"
+          images="/img/Developpement/vite+react.png"
+          reverse={true}
+          buttons={[
+              {
+              label: "Voir le projet",
+              href: "https://github.com/gabriel-mockers-nws/Portfolio-Gmockers-vite",
+              iconClass: "fa-brands fa-github",
+              isExternal: true
+              },
+              {
+                label: "Voir plus de projets developpement",
+                href: "/DevProjects",
+                }
+          ]}
+          >
+            Dans cette partie je vous présente tous mes projet de développement web. <br /> 
+      </Article>
 
     </>
   );
